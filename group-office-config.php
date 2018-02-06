91fbc67a5b22f64069da884dcbc5e2e70319cd1d
Christopher Morrissey <cmorrissey@linnovation.com>
2018-02-06 17:53:24 -0500

    Add files via upload
    push Dockerfile and init.sh
    
    should work  idk

---
+++ group-office-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Group-Office configuration file.
+ * Visit https://www.group-office.com/wiki/Configuration_file for available values
+ */
+$config["default_timezone"]='America/New_York';
+$config["host"]='/';
+$config["title"]='Group-Office';
+$config["root_path"]='/var/www/html/';
+$config["language"]='en';
+$config["default_country"]='US';
+$config["default_date_separator"]=',';
+$config["default_time_format"]='H:i a';
+$config["register_user_groups"]='Intern';
+$config["register_visible_user_groups"]='Intern';
